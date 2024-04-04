b4eacd80417be104dccfd08e204c333416df36ec
anshchovatiya <sqanshchovatiya800@gmail.com>
2024-04-04 09:19:23 +0530

    new

--- src/controller/user/order.controller.js
+++ src/controller/user/order.controller.js
@@ -1,4 +1,4 @@
-const OrderServices = require('../../Services/order.service');
+const OrderServices = require('../../services/order.service');
 const orderService = new OrderServices();
 const CartServices = require('../../services/cart.service');
 const cartService = new CartServices();
